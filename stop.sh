486224350b1381393f893ec77e0e73f96f6b73ad
Louis Stewart <ls99@st-andrews.ac.uk>
2018-04-05 16:05:46 +0100

    Spark connectors added

---
+++ stop.sh
@@ -0,0 +1,20 @@
+SPARK_URL=$1
+echo "Spark url: " $1
+COUNTER=0
+HOSTS=()
+for i in $(seq -f "%03g" 1 150)
+do
+  test=pc2-${i}-l.cs.st-andrews.ac.uk
+  status=$(ssh -o BatchMode=yes -o ConnectTimeout=5 $test echo ok 2>&1)
+
+  if [[ $status == ok && ${#HOSTS[@]} -lt 20 ]] ; then # If host alive, add to hosts array
+    COUNTER=$((COUNTER+1))
+    HOSTS+=($test)
+    echo "Connected to: " $test " Count: " ${#HOSTS[@]}
+    ssh $test 'cd ~/Downloads/spark-master; ./sbin/stop-slave.sh '
+    echo "Starting slave on host"
+  elif [[ ${#HOSTS[@]} -eq 20 ]] ; then
+    echo "Got enough hosts"
+    break
+  fi
+done
