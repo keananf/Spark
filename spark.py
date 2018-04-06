c6dac27dbb85adb939d8f12ae66a2e41185f91d3
Louis Stewart <ls99@st-andrews.ac.uk>
2018-04-06 12:14:43 +0100

    Clean up repo.

--- spark.py
+++
@@ -1,9 +0,0 @@
-from pyspark import SparkContext, SparkConf
-from pyspark.sql import SparkSession, SQLContext
-
-
-remote_conf = SparkConf().setAppName('sparkTest').setMaster('spark://pc2-119-l.cs.st-andrews.ac.uk:7077')
-local_conf = SparkConf().setAppName('sparkTest').setMaster('local')
-
-sparkContext = SparkContext(conf=local_conf)
-sparkSql = SparkSession.builder.config(conf=local_conf).getOrCreate()
