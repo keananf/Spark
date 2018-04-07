bd33d3b3c7b3f4dfa7778cccd2ed54a994c7fd6b
Louis Stewart <ls99@st-andrews.ac.uk>
2018-04-07 15:52:40 +0100

    Refactor interaction with spark into method, so that it doesn't run when loading module.

--- src/test.py
+++ src/test.py
@@ -2,9 +2,6 @@ from src.spark import SparkInteraction
 from pyspark.sql.types import StringType, StructField, StructType, ArrayType, LongType
 
 
-spark = SparkInteraction('test', 'spark://pc5-035-l.cs.st-andrews.ac.uk')
-sess = spark.session()
-
 schema = StructType([
     StructField("text", StringType(), True),
     StructField('entities', StructType([StructField('hashtags', ArrayType(StructType([StructField('indices', ArrayType(LongType(), True), True), StructField('text', StringType(), True)]), True), True)]), True),
@@ -15,4 +12,8 @@ schema = StructType([
     StructField('user', StructType([StructField('followers_count', LongType(), True), StructField('utc_offset', LongType(), True)]))
 ])
 
-df = sess.read.json("file:///cs/unique/ls99-kf39-cs5052/data/tweets/*.json", schema=schema)
+
+def load_datframe():
+    spark = SparkInteraction('test', 'spark://pc5-035-l.cs.st-andrews.ac.uk:7077')
+    sess = spark.session()
+    df = sess.read.json("file:///cs/unique/ls99-kf39-cs5052/data/tweets/*.json", schema=schema)
