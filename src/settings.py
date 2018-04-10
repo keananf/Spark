a8fb7289d34caf1b18003cf9241a22cd96a3843e
Louis Stewart <ls99@st-andrews.ac.uk>
2018-04-10 14:56:01 +0100

    Fix file directories in settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,6 @@
 import os
 
-PROJECT_ROOT = os.path.abspath(os.path.join(os.path.abspath(__file__), os.pardir))
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 DATA_DIR = os.path.join(PROJECT_ROOT, "data")
 CRYPTO_DIR = os.path.join(DATA_DIR, "crypto")
 IMG_DIR = os.path.join(PROJECT_ROOT, "img")
