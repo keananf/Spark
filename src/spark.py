df9f990d3cc4dc92872a927075156654470be3a2
Louis Stewart <ls99@st-andrews.ac.uk>
2018-04-11 18:22:50 +0100

    New sentiment training set loading and analysis began.

--- src/spark.py
+++ src/spark.py
@@ -23,5 +23,5 @@ class Spark(object):
         Create spark SQL Session - useful for structuring unstructured data, e.g. JSON
         :return: SQL Session
         """
-        return SparkSession.builder.config(conf=self.conf).config('spark.jars', "/cs/home/kf39/Downloads/spark-nlp_2.11-1.5.0.jar").getOrCreate()
+        return SparkSession.builder.config(conf=self.conf).config('spark.jars', "/cs/unique/ls99-kf39-cs5052/sparknlp.jar").getOrCreate()
 
